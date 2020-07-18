3544f4f7891ac68820a1151924c4c508516c0f68
Riccardo Zulian <riccardo.zulian@gmail.com>
2020-07-18 15:38:41 +0200

    Industry tokens

--- src/data/industries.ts
+++ src/data/industries.ts
@@ -2,8 +2,13 @@ import IndustryType from "../enums/industries";
 import Industry from "../industry";
 
 const industries: Industry[] = [
-  { name: "coal1", type: IndustryType.CoalMine, level: 1, cost: { money: 12}, production: {coal: 4}}
-
+  { name: "coal1", type: IndustryType.CoalMine, level: 1, cost: { money: 12}, production: {coal: 4}},
+  { name: "coal2", type: IndustryType.CoalMine, level: 2, cost: { money: 12}, production: {coal: 4}}
 ];
 
-export default industries;
+const industrySet: Array<{num: number, industry: string}> = [
+  { num: 3, industry: "coal1" },
+  { num: 3, industry: "coal2" }
+];
+
+export {industries, industrySet};
